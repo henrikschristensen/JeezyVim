c6d44b6fb077a0cd7d1f331b7cc5277a8a118e32
System administrator <root@nixos.jbmain00.corp.jyskebank.net>
2024-12-21 10:49:56 +0000

    remove rust

--- config/plugins/plugins.nix
+++ config/plugins/plugins.nix
@@ -4,9 +4,8 @@
       enable = pkgs.lib.mkDefault true;
       settings.checkTs = pkgs.lib.mkDefault true;
     };
-    crates-nvim.enable = pkgs.lib.mkDefault true;
+    crates.enable = pkgs.lib.mkDefault true;
     bacon.enable = pkgs.lib.mkDefault true;
-    rust-tools.enable = pkgs.lib.mkDefault true;
     lastplace.enable = pkgs.lib.mkDefault true;
     todo-comments = {
       enable = pkgs.lib.mkDefault true;
@@ -47,13 +46,13 @@
         lsp.autoAttach = pkgs.lib.mkDefault true;
       };
     };
-    nvim-colorizer.enable = pkgs.lib.mkDefault true;
+    nvim.enable = pkgs.lib.mkDefault true;
     marks.enable = pkgs.lib.mkDefault true;
     vim-matchup = {
-      enable = pkgs.lib.mkDefault true;
-      enableSurround = pkgs.lib.mkDefault true;
-      enableTransmute = pkgs.lib.mkDefault true;
-      treesitterIntegration.enable = pkgs.lib.mkDefault true;
+      enable = pkgs.lib.mkDefault true
+      treesitter.enable = pkgs.lib.mkDefault true;
+      settings.transmute_enabled = pkgs.lib.mkDefault true;
+      settings.surround_enabled = pkgs.lib.mkDefault true;     
     };
     web-devicons.enable = true;
     copilot-vim = {
